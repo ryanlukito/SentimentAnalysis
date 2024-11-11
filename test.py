beb09baae79d21f1d0c8d0a84879989a3f614dbd
alexchtst-01 <acinatra@gmail.com>
2024-11-11 17:15:02 +0700

    feat: add how to use the datapreprocessing

---
+++ test.py
@@ -0,0 +1,11 @@
+from datapreprocessing import DataPreprocessing
+
+teguh_barata_aji = DataPreprocessing(filename='data/horeg.csv')
+
+
+# labeling = teguh_barata_aji.labellingData(5)
+# labeling.to_csv('label.csv', index=False)
+
+data_cleaned = teguh_barata_aji.clean_text('label.csv')
+
+print(data_cleaned)
