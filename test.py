e62cab0cdaeb441c7e78d3e9a34c2e81d06d8124
alexchtst-01 <acinatra@gmail.com>
2024-11-13 17:38:57 +0700

    move to branch alex

--- test.py
+++ test.py
@@ -1,11 +1,13 @@
 from datapreprocessing import DataPreprocessing
+import pandas as pd
 
-teguh_barata_aji = DataPreprocessing(filename='data_horeg_alex.csv')
+print(pd.read_csv('label.csv')['label'].value_counts())
 
+# teguh_barata_aji = DataPreprocessing(filename='data_alex.csv')
 
-# labeling = teguh_barata_aji.labellingData(5)
+# labeling = teguh_barata_aji.labellingData()
 # labeling.to_csv('label.csv', index=False)
 
-data_cleaned = teguh_barata_aji.clean_text('label.csv')
+# data_cleaned = teguh_barata_aji.clean_text('label.csv')
 
-print(data_cleaned)
+# print(data_cleaned)
